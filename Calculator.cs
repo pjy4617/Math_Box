4e0ad342a85ed0b823aa7868b802c0626f74a166
pjy4617_ <jypark.t1@gmail.com>
2020-08-31 23:00:22 +0900

    Create MathTool

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTool
+{
+    
+    class Calculator
+    {
+        //께산시 프로그램
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
